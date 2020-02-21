1e25eacea797949fedb489487a3dd89c37e37c37
Freddy <freddy.artea@gmail.com>
2020-02-21 18:21:18 -0500

    mostrando las categorias en el select

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,9 +4,19 @@ import useSelect from '../hooks/useSelect'
 
 const Formulario = () => {
 
+    const OPCIONES = [
+        { value: 'general', label: 'General' },
+        { value: 'busines', label: 'Negocios' },
+        { value: 'entertainment', label: 'Entretenimiento' },
+        { value: 'health', label: 'Salud' },
+        { value: 'science', label: 'Ciencia' },
+        { value: 'sports', label: 'Deportes' },
+        { value: 'technology', label: 'Tecnologia' },
+    ]
+
     // utilizar custom hook
 
-    const [categoria, SelectNoticias] = useSelect()
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES)
 
     return ( 
         <div className="row buscador">
